fa10cb41f98fbf1eb9d4acbefa47e002686e0bd4
pulind2004 <pulind@sina.com>
2017-03-07 10:48:54 +0800

    修改测试样例

--- sample_stock/db/db_test.py
+++ sample_stock/db/db_test.py
@@ -2,30 +2,8 @@
 # -*- coding:utf-8 -*- 
 # Author: pulin
 
-from db_pool import getPTConnection as db
-import traceback
 
-def TestMySQL():
-    # SQL 查询语句;
-    sql = "SELECT * FROM auth_user";
-    try:
-        # 获取所有记录列表
-        db.cursor.execute(sql)
-        results = db.cursor.fetchall();
-        for row in results:
-            userId = row[0]
-            password = row[1]
-            name = row[4]
-            createTime = row[10]
-            # 打印结果
-            print ("userId=%d,password=%s,sex=%s,createTime=%s" %(userId, password, name, createTime ))
-    except:
-        print ("Error: unable to fecth data")
-        traceback.print_exc()
-
-TestMySQL()
-
-from MySqlConn import Mysql
+from db_pool import Mysql
 from _sqlite3 import Row
 
 # 申请资源
