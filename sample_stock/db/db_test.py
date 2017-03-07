c143a4bb0d0874d17b41c0ee1c717f2d14941d3f
pulind2004 <pulind@sina.com>
2017-03-07 10:45:36 +0800

    修改测试样例

--- sample_stock/db/db_test.py
+++ sample_stock/db/db_test.py
@@ -24,3 +24,28 @@ def TestMySQL():
         traceback.print_exc()
 
 TestMySQL()
+
+from MySqlConn import Mysql
+from _sqlite3 import Row
+
+# 申请资源
+mysql = Mysql()
+
+sqlAll = "SELECT * FROM settings_item"
+result = mysql.getAll(sqlAll)
+if result:
+    print "get all"
+    for row in result:
+        print "%s\t%s" % (row["id"], row["remark"])
+result = mysql.getMany(sqlAll, 1)
+if result:
+    print "get many"
+    for row in result:
+        print "%s\t%s" % (row["id"], row["remark"])
+
+result = mysql.getOne(sqlAll)
+print "get one"
+print "%s\t%s" % (result["id"], result["remark"])
+
+# 释放资源
+mysql.dispose()
